fce4d428470fb8019c5902ff2f7d84c297c772e2
Chloe <cy214@sussex.ac.uk>
2021-11-24 18:14:31 +0000

    Added HUD

---
+++ Prog3D_Coursework/Assets/voice_Call.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class voice_Call : MonoBehaviour
+{
+    [SerializeField] private AudioClip IntroAudio;
+    [SerializeField] private AudioClip checkChute;
+    [SerializeField] private AudioClip checkUpstairs;
+    [SerializeField] private AudioClip headForExit;
+    [SerializeField] private AudioClip foundKeyBox;
+    [SerializeField] private AudioClip foundGrenade;
+    [SerializeField] private AudioClip finishedJob;
+    [SerializeField] private AudioClip carefulRobots;
+    [SerializeField] private AudioClip foundSystem;
+    [SerializeField] private AudioClip uhOhAlarm;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
