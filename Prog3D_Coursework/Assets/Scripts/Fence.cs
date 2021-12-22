d1e800500c64e0c256d7374da4c36dc362e7eb18
Chloe <cy214@sussex.ac.uk>
2021-12-22 13:58:16 +0000

    Fence already played audio

--- Prog3D_Coursework/Assets/Scripts/Fence.cs
+++ Prog3D_Coursework/Assets/Scripts/Fence.cs
@@ -7,12 +7,17 @@ using UnityEngine.Events;
 public class Fence : MonoBehaviour
 {
     [SerializeField] private UnityEvent voiceCallEvent;
+	private bool hasAlreadyPlayed = false;
     // Start is called before the first frame update
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+	if(!hasAlreadyPlayed)
+{
             voiceCallEvent.Invoke();
+		hasAlreadyPlayed = true;
+}
             if (GameManager.Instance.KeyboxComplete)
             {
                 if (Input.GetKeyDown(KeyCode.E))
